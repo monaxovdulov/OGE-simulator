916de1e1b2ddf115b5ea6f408fb907dfe38aa253
Roman <35300872+monaxovdulov@users.noreply.github.com>
2024-09-04 22:53:45 +0300

    Update test_find_removed_word.py

--- test_find_removed_word.py
+++ test_find_removed_word.py
@@ -3,4 +3,4 @@ from main import find_removed_word
 
 def test_find_removed_word():
     sentence = "Ложка, лес, комбайн, льдина, обезьяна, микроволновка, видеокарта – это слова"
-    assert find_removed_word(sentence) == "микроволновка"
+    assert find_removed_word(sentence) == "льдина"
